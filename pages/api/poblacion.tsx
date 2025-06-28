65bded4060e677630b3298a5db0128b2e908dd0f
Derty15 <113853283+Derty15@users.noreply.github.com>
2025-06-28 15:57:33 -0600

    Update poblacion.tsx

--- pages/api/poblacion.tsx
+++ pages/api/poblacion.tsx
@@ -5,7 +5,7 @@ export const config = {
 };
 
 export default async function handler() {
-  const res = await fetch("https://www.inegi.org.mx/app/api/indicadores/desarrolladores/json/INDICATOR/6200042410/es/00000/false/BIE/2.0/?token=b55cfc56-efff-64fa-7ab0-88938cd3d197");
+  const res = await fetch("https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/6200205260/es/0700/true/BISE/2.0/b55cfc56-efff-64fa-7ab0-88938cd3d197?type=json");
   const json = await res.json();
   const poblacion = Number(json.Series[0].OBSERVATIONS[0].OBS_VALUE).toLocaleString("es-MX");
 
