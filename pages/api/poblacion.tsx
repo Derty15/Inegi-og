528cef93b1a8efccfd85cb6a51ecc99f748b4128
Derty15 <113853283+Derty15@users.noreply.github.com>
2025-06-28 15:41:07 -0600

    Create poblacion.tsx

---
+++ pages/api/poblacion.tsx
@@ -0,0 +1,37 @@
+import { ImageResponse } from '@vercel/og';
+
+export const config = {
+  runtime: 'edge',
+};
+
+export default async function handler() {
+  const res = await fetch("https://www.inegi.org.mx/app/api/indicadores/desarrolladores/json/INDICATOR/6200042410/es/00000/false/BIE/2.0/?token=b55cfc56-efff-64fa-7ab0-88938cd3d197");
+  const json = await res.json();
+  const poblacion = Number(json.Series[0].OBSERVATIONS[0].OBS_VALUE).toLocaleString("es-MX");
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: '#f0f0f0',
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 42,
+          fontWeight: 'bold',
+          color: '#222',
+        }}
+      >
+        <div>Población estimada en México</div>
+        <div>{poblacion}</div>
+      </div>
+    ),
+    {
+      width: 800,
+      height: 400,
+    }
+  );
+}
