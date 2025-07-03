b48eb2459dc441e4c98ba31c39312ad1f14ef0c1
Derty15 <113853283+Derty15@users.noreply.github.com>
2025-07-02 19:58:28 -0600

    Update poblacion.tsx

--- pages/api/poblacion.tsx
+++ pages/api/poblacion.tsx
@@ -5,7 +5,7 @@ export const config = {
 };
 
 export default async function handler() {
-  const res = await fetch("https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/6200205260/es/0700/true/BISE/2.0/b55cfc56-efff-64fa-7ab0-88938cd3d197?type=json");
+  const res = await fetch("https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/6200028409/es/0700/true/BISE/2.0/b55cfc56-efff-64fa-7ab0-88938cd3d197?type=json");
   const json = await res.json();
   const poblacion = Number(json.Series[0].OBSERVATIONS[0].OBS_VALUE).toLocaleString("es-MX");
 
@@ -25,7 +25,7 @@ export default async function handler() {
           color: '#222',
         }}
       >
-        <div>Población estimada en México</div>
+        <div>Tasa de incidencia delictiva por robo o asalto en la calle o transporte público por cada cien mil habitantes</div>
         <div>{poblacion}</div>
       </div>
     ),
